8395efd63a10b565e0051bf80eed21f1609afb85
Davi <davifsc03@gmail.com>
2025-09-24 21:13:26 -0300

    Atividade customizada configurada

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,9 +1,13 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
+        client.user.setActivity({
+            type: ActivityType.Custom,
+            name: 'observando silenciosamente'
+        });
         console.log(`O Aplicativo ${client.user.username} está pronto para uso`);
     }
 }

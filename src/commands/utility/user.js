e878264319c8526bf5c323bd7c6d0c636fae1e88
Davi <davifsc03@gmail.com>
2025-09-24 23:14:11 -0300

    Comando para exibir as informações sobre um membro do servidor

--- src/commands/utility/user.js
+++ src/commands/utility/user.js
@@ -1,8 +1,59 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, MessageFlags, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('user').setDescription('Exibe informações sobre um membro do servidor'),
+    data: new SlashCommandBuilder()
+        .setName('user')
+        .setDescription('Exibe informações sobre um membro do servidor')
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageNicknames)
+        .addUserOption(option => option
+            .setName('apelido')
+            .setDescription('Informe o apelido do membro que deseja saber os detalhes')
+            .setRequired(true)
+        ),
     async execute(interaction) {
-        await interaction.reply(`Esse comando foi executado por ${interaction.user.username} que entrou neste servidor em ${interaction.member.joinedAt}`,);
+        const user = interaction.options.getUser('apelido');
+        const userAllData = await user.fetch();
+        const member = await interaction.guild.members.fetch(userAllData.id);
+        const roles = member.roles.cache.map(role => role).join(` \n `);
+        const avatar = userAllData.displayAvatarURL({
+            dynamic: true,
+            size: 512,
+        });
+        const banner = userAllData.bannerURL({
+            dynamic: true,
+            size: 1024,
+        });
+        const joinedAt = `<t:${parseInt(member.joinedAt / 1000)}:R>`;
+        const createdAt = `<t:${parseInt(user.createdAt / 1000)}:R>`;
+
+        const embed = new EmbedBuilder()
+            .setTitle(`${userAllData.username}`)
+            .setDescription(`Detalhes sobre ${userAllData.tag}`)
+            .setColor('Random')
+            .setThumbnail(avatar)
+            .setImage(banner)
+            .setFooter({ text: `ID: ${userAllData.id}` })
+            .setTimestamp()
+            .addFields(
+                {
+                    name: 'Entrou no servidor',
+                    value: `${joinedAt}`,
+                    inline: true
+                },
+                {
+                    name: 'Data da conta',
+                    value: `${createdAt}`,
+                    inline: true
+                },
+                {
+                    name: 'Cargos',
+                    value: `${roles}`,
+                    inline: true
+                },
+            );
+        await interaction.reply({
+            embeds: [embed],
+            flags: MessageFlags.Ephemeral
+        });
     }
 }
